64dc8e977cf0fda884f3b8385cd4095fdb9cf368
conor-v <conor.vanoystaeyen@hotmail.com>
2022-05-31 17:45:31 +0200

    config test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSwipeable} from 'react-swipeable'
+import { useState } from 'react';
+
+const config = {
+  delta: 10,                             // min distance(px) before a swipe starts. *See Notes*
+  preventScrollOnSwipe: false,           // prevents scroll during swipe (*See Details*)
+  trackTouch: true,                      // track touch input
+  trackMouse: true,                     // track mouse input
+  rotationAngle: 0,                      // set a rotation angle
+  swipeDuration: Infinity,               // allowable duration of a swipe (ms). *See Notes*
+  touchEventOptions: { passive: true },  // options for touch listeners (*See Details*)
+} 
 
 function App() {
+  const [swipe, setSwipe] = useState('???')
+  const handlers = useSwipeable({
+    onSwipedLeft: (eventData) => nextSlide('left'),
+    onSwipedRight: (eventData) => nextSlide('right'),
+    ...config,
+  })
+
+  const nextSlide = (swipe) => {
+    let direction;
+
+    if (swipe === "left") {
+      direction = "TO THE LEFT"
+    }
+    if (swipe === "right") {
+      direction = "TO THE RIGHT"
+    }
+
+    setSwipe(direction)
+  }
+
   return (
-    <div className="App">
+    <div className="App" {...handlers}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>{swipe}</h1>
       </header>
     </div>
   );
